38b6b4930cdd40857f92ea4739a7ceacdc96ee4b
Sebastian Koenig <sebastian.koenig@com-plus-it.de>
2023-03-21 10:36:33 +0100

    new@dev: updated readme images and included mobile example

--- flutter_responsive_example/lib/main.dart
+++ flutter_responsive_example/lib/main.dart
@@ -3,7 +3,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_navbar/responsive_navbar.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -18,13 +17,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: DesktopMain(title: 'Flutter Demo Home Page'),
+      home: MobileMain(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class DesktopMain extends StatefulWidget {
-
   DesktopMain({super.key, required this.title, this.index = 0});
 
   final String title;
@@ -82,7 +80,7 @@ class _DesktopMainState extends State<DesktopMain> {
                 barCircularity: 0,
                 iconPadding: const EdgeInsets.only(bottom: 5, top: 5),
                 barThickness: 5,
-                usingRightBar: true,
+                usingBottomBar: true,
                 isForDesktop: true,
                 currentIndex: currentIndex,
                 onTap: (index) => _handleIndexChanged(index),
@@ -140,5 +138,119 @@ class _DesktopMainState extends State<DesktopMain> {
         ),
       ),
     );
+  }
+}
+
+class MobileMain extends StatefulWidget {
+  MobileMain({super.key, required this.title, this.index = 0});
+
+  final String title;
+  int index = 0;
+
+  @override
+  State<MobileMain> createState() => _MobileMainState();
+}
+
+class _MobileMainState extends State<MobileMain> {
+  final ValueNotifier<int> _currentIndex = ValueNotifier(0);
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(initialPage: widget.index);
+    _currentIndex.value = widget.index;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
+//##############################################################################
+
+  void _handleIndexChanged(int index) {
+    if (_currentIndex.value != index) {
+      pageController
+          .jumpToPage(_currentIndex.value < index ? index - 1 : index + 1);
+      pageController.animateToPage(index,
+          duration: const Duration(milliseconds: 700),
+          curve: Curves.easeInOutQuart);
+      _currentIndex.value = index;
+    }
+  }
+
+//##############################################################################
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      color: const Color(0xFF131313),
+      child: Column(
+        children: [
+          Expanded(
+            //################################################################
+            //PageView
+            //################################################################
+            child: PageView(
+              physics: const NeverScrollableScrollPhysics(),
+              scrollDirection: Axis.vertical,
+              controller: pageController,
+              onPageChanged: (index) => _currentIndex.value = index,
+              children: const <Widget>[
+                Placeholder(),
+                Placeholder(),
+                Placeholder(),
+                Placeholder(),
+                Placeholder(),
+              ],
+            ),
+          ),
+          ValueListenableBuilder(
+            valueListenable: _currentIndex,
+            builder: (context, dynamic currentIndex, _) => ResponsiveBar(
+              useBarAnimation: true,
+              iconPadding: const EdgeInsets.all(13),
+              usingTopBar: true,
+              isForDesktop: false,
+              currentIndex: currentIndex,
+              onTap: _handleIndexChanged,
+              iconSize: 30,
+              scaling: 60,
+              transition: const Duration(milliseconds: 500),
+              iconScaleAnimationFactor: 0.5,
+              iconScaleCurve: Curves.easeOutSine,
+              barSizeCurve: Curves.decelerate,
+              barAccentColor: Colors.white,
+              backgroundColor: Colors.black38,
+              activeItemColor: Colors.white,
+              passiveItemColor: Colors.white38,
+              items: [
+                //######################################################
+                //BarItems
+                //######################################################
+                ResponsiveBarItem(
+                    icon: const Icon(Icons.store_outlined),
+                    selectedIcon: const Icon(Icons.store_rounded)),
+                ResponsiveBarItem(
+                    icon: const Icon(Icons.badge_outlined),
+                    selectedIcon: const Icon(Icons.badge_rounded)),
+                ResponsiveBarItem(
+                    icon: const Icon(Icons.vpn_key_outlined),
+                    selectedIcon: const Icon(Icons.vpn_key_rounded)),
+                ResponsiveBarItem(
+                    icon: const Icon(Icons.source_outlined),
+                    selectedIcon: const Icon(Icons.source_rounded)),
+                ResponsiveBarItem(
+                    icon: const Icon(Icons.linear_scale_outlined),
+                    selectedIcon: const Icon(Icons.linear_scale_rounded))
+              ],
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
